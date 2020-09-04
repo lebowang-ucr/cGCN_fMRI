6b094e28d6083ad5fc3ccffdb7fe44fca5531cc2
Lebo Wang <whswlb@gmail.com>
2020-09-04 16:09:19 -0700

    update HCP

--- model.py
+++ model.py
@@ -9,11 +9,9 @@ import numpy as np
 from keras.layers import Layer, InputSpec
 from keras import initializers, regularizers, constraints
 
-ROI_N = 236
-
 def T_get_edge_feature(point_cloud_series, nn_idx, k=5):
 #     """Construct edge feature for each point
-#     refer to https://github.com/WangYueFt/dgcnn/blob/master/tensorflow/utils/tf_util.py
+#     Please refer to https://github.com/WangYueFt/dgcnn/blob/master/tensorflow/utils/tf_util.py
 #     Args:
 #     point_cloud_series: (batch_size, time_step, num_points, 1, num_dims)
 #                      or (batch_size, time_step, num_points   , num_dims)
@@ -36,22 +34,31 @@ def T_get_edge_feature(point_cloud_series, nn_idx, k=5):
     num_points = point_cloud_shape[-2].value
     num_dims = point_cloud_shape[-1].value
 
-    # edge features: (batch_size, time_step, num_points, k, num_dims)
+    # Shared graph for all subjects in the batch and all time-frames
     nn_idx = tf.expand_dims(nn_idx, axis=1)
-    nn_idx = tf.tile(nn_idx, [1, time_step, 1, 1])
+    
+    # Create the shared graph
+
+    # Copy the neighborhood definition for each time step
+    nn_idx = tf.tile(nn_idx, [1, time_step, 1, 1]) # https://www.tensorflow.org/api_docs/python/tf/tile
     nn_idx = tf.cast(nn_idx, dtype=tf.int32)
 
+    # Create the shared graph for all batches
     idx_ = tf.range(batch_size*time_step) * num_points
     idx_ = tf.reshape(idx_, [batch_size, time_step, 1, 1]) 
     idx_ = tf.cast(idx_, dtype=tf.int32)
 
     point_cloud_flat = tf.reshape(point_cloud_series, [-1, num_dims])
+    # point_cloud_neighbors defined by k-NN
     point_cloud_neighbors = tf.gather(point_cloud_flat, nn_idx+idx_)
-    # point_cloud_neighbors
     point_cloud_central = tf.expand_dims(point_cloud_central, axis=-2)
 
+    # Copy the central point data for k times
     point_cloud_central = tf.tile(point_cloud_central, [1, 1, 1, k, 1])
 
+    # For each neighbor, one dimension is x_i as the global features,
+    # the difference between neighbors and the central point: x_j - x_i, as the local interaction
+    # Therefore, feature * 2
     edge_feature = tf.concat([point_cloud_central, point_cloud_neighbors-point_cloud_central], axis=-1)
     return edge_feature
 
@@ -61,24 +68,29 @@ def T_conv_bn_max(edge_feature, kernel=2, activation_fn='relu'):
 #     edge_feature: (batch_size, time_step, num_points, k, num_dims)
 #     kernel: conv kernel units
 #     activation_fn: non-linear activation
+#
+#     Returns:
+#     edge features: (batch_size, time_step, num_points, 1, kernel)
 
-    ######### Conv2D #########
     net = TimeDistributed(Conv2D(kernel, (1,1)))(edge_feature)
-    # net = TimeDistributed(BatchNormalization(axis=-1))(net)
+    # net = TimeDistributed(BatchNormalization(axis=-1))(net) # BatchNorm, can be enabled
     if activation_fn is not None:
         net = TimeDistributed(Activation(activation_fn))(net)
     return TimeDistributed(Lambda(lambda x: tf.reduce_max(x, axis=-2, keep_dims=True)))(net)
 
 def T_edge_conv(point_cloud_series, graph, kernel=2, activation_fn='relu', k=5):
-#     """TimeDistributed conv with max as aggregation
-#     """Wrapper for T_get_edge_feature and T_edge_conv
+#     """TimeDistributed conv with max as aggregation,
+#        wrapper for T_get_edge_feature and T_edge_conv
 #     Args:
 #     point_cloud_series: (batch_size, time_step, num_points, 1, num_dims)
 #                      or (batch_size, time_step, num_points   , num_dims)
-#     graph (FC graph): (num_points, k)
+#     graph (FC matrix): (num_points, k)
 #     kernel: conv kernel units
 #     activation_fn: non-linear activation
 #     k: no. of neighbors for cGCN
+#
+#     Returns:
+#     edge features: (batch_size, time_step, num_points, 1, kernel)
 
     # assert len(graph.get_shape().as_list()) == 2
     graph = Lambda(lambda x: tf.tile(tf.expand_dims(x[0], axis=0), 
@@ -90,8 +102,9 @@ def T_edge_conv(point_cloud_series, graph, kernel=2, activation_fn='relu', k=5):
 
 ######################## Model description ########################
 
-def get_model(graph_path='FC.npy',
-    input_frame=100, kernels=[8,8,8,16,32,32], 
+def get_model(graph_path,
+    ROI_N, frames, 
+    kernels=[8,8,8,16,32,32], 
     k=5, l2_reg=1e-4, dp=0.5,
     num_classes=100,
     weight_path=None, skip=[0,0]):
@@ -101,20 +114,25 @@ def get_model(graph_path='FC.npy',
     graph = adj_matrix.argsort(axis=1)[:, ::-1][:, 1:k+1]
 
     ############ define model ############
-    main_input = Input((input_frame, ROI_N, 1), name='points')
+    main_input = Input((frames, ROI_N, 1), name='points')
     static_graph_input = Input(tensor=tf.constant(graph, dtype=tf.int32), name='graph')
 
     # 5 conv layers
+
+    # 4 stacking conv layers
     net1 = T_edge_conv(main_input, graph=static_graph_input, kernel=kernels[0], k=k)
     net2 = T_edge_conv(net1, graph=static_graph_input, kernel=kernels[1], k=k)
     net3 = T_edge_conv(net2, graph=static_graph_input, kernel=kernels[2], k=k)
     net4 = T_edge_conv(net3, graph=static_graph_input, kernel=kernels[3], k=k)
     net = Lambda(lambda x: tf.concat([x[0], 
         x[1], x[2], x[3]], axis=-1))([net1, net2, net3, net4])
+
+    # 1 final conv layer with shortcuts from previous conv layers
     net = T_edge_conv(net, graph=static_graph_input, kernel=kernels[4], k=k)
     
     net = TimeDistributed(Dropout(dp))(net)
-    # ConvLSTM2D layer for temporal info
+    # ConvLSTM2D layer for temporal info, bettern than RNN
+    # L2 reg for recurrent parameters for easy convergence
     net = ConvLSTM2D(kernels[5], kernel_size=(1,1), padding='same', 
                    return_sequences=True, recurrent_regularizer=l2(l2_reg))(net)
     net = TimeDistributed(BatchNormalization())(net)
@@ -154,9 +172,9 @@ if __name__ == "__main__":
     np.save('FC_random', random_FC)
     
     N = 50
-    n_frames = 100
-    x_train = np.random.normal(0, 1, size=(N, n_frames, ROI_N, 1))
-    x_test = np.random.normal(0, 1, size=(N, n_frames, ROI_N, 1))
+    frames = 100
+    x_train = np.random.normal(0, 1, size=(N, frames, ROI_N, 1))
+    x_test = np.random.normal(0, 1, size=(N, frames, ROI_N, 1))
     print('train data shape:', x_train.shape) # train data shape: (50, 100, 236, 1)
     print('test data shape:', x_test.shape) # test data shape: (50, 100, 236, 1)
 
@@ -168,7 +186,9 @@ if __name__ == "__main__":
         print('Label %d: %d/%d (%.1f%%)'%(label, count, y_train.shape[0], 100.0 * count / y_train.shape[0]))
 
     model = get_model(
-        graph_path='FC_random.npy', 
+        graph_path='FC_random.npy',
+        ROI_N=ROI_N,
+        frames=frames, 
         kernels=[8,8,8,16,32,32], 
         k=3, 
         l2_reg=0, 
